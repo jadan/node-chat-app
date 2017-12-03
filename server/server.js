e5c6c3b3bcf375b66a93b822024048092ddd6b9f
Jose Mendoza <josemendoza@Joses-MacBook-Pro.local>
2017-12-03 15:41:49 -0700

    added sockets

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
+//server side javascript
+
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 const express = require('express');
 
 
@@ -6,8 +10,36 @@ const publicPath = path.join(__dirname, '../public');
 
 const  port = process.env.PORT || 3000; 
 var app = express();
+var server = http.createServer(app);
+
+//communicate between server and client
+var io = socketIO(server);
+
+io.on('connection', (socket)=>{
+	console.log('New user connected.');
+
+	// socket.emit('newMessage', {
+	// 	from: 'mike@example.com',
+	// 	text: 'hey what\'s going on!',
+	// 	createAt: 123
+	// });
+
+	socket.on('disconnect', ()=>console.log('User was diconnected.'));
+
+	socket.on('createMessage', (message)=> {
+		console.log(message);
+		//socket.emit to single connection, io.emit to every connection
+		io.emit('newMessage', {
+			from:message.from,
+			text:message.text,
+			createAt: new Date().getTime()
+		});
+	});
+});
+
+
 
 //Middleware (express) Third-party add-on. 
 app.use(express.static(publicPath));
 
-app.listen(port, ()=>{console.log(`Server is up on port ${port}.`)});
+server.listen(port, ()=>{console.log(`Server is up on port ${port}.`)});
