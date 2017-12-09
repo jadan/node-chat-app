e32b6e85445ac5194b7f21e452532cf1377bd528
Jose Mendoza <josemendoza@Joses-MacBook-Pro.local>
2017-12-09 15:41:57 -0700

    added autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 //client side javascript
 var socket = io(); 
 
+function scrollToBottom(){
+	// selectors
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+
+	// heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight+scrollTop + newMessageHeight + lastMessageHeight>= scrollHeight){
+		// console.log('should scroll');
+		messages.scrollTop(scrollHeight);
+	}
+};
 
 socket.on('connect', function(){
 	console.log('Connected to server.');
@@ -23,6 +40,7 @@ socket.on('newMessage', function(message){
 		createAt: formattedTime
 	});
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message){
@@ -36,7 +54,7 @@ socket.on('newLocationMessage', function(message){
 	});
 
 	jQuery('#messages').append(html);
-	
+	scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
